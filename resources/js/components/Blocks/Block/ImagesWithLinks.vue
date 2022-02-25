9fdb06ef8d00b3f3bdceec67c8cf71ee7742e72d
Andrei Ioniță <hi@andrei.io>
2022-02-25 22:48:37 +0000

    feat: images with links

---
+++ resources/js/components/Blocks/Block/ImagesWithLinks.vue
@@ -0,0 +1,34 @@
+<template>
+    <localized-field
+        field="form-input"
+        :label="$t('field.title')"
+        v-model="content.title"
+    />
+
+    <form-input
+        type="number"
+        :label="$t('field.columns')"
+        v-model.number="content.columns"
+        :min="1"
+        :max="5"
+    />
+
+    <block-repeater
+        component="image-with-link"
+        v-model="children"
+        v-model:media="media"
+    />
+</template>
+
+<script>
+    import { defineBlock } from '@/helpers';
+
+    export default defineBlock({
+        type: 'images-with-links',
+        icon: 'Media/image-add-line',
+        fields: {
+            title: Object,
+            columns: Number,
+        },
+    });
+</script>
---
+++ resources/views/components/blocks/images-with-links.blade.php
@@ -0,0 +1,9 @@
+<x-blocks._title :title="$title" />
+
+<div @class(['grid gap-8', $columns])>
+    @foreach ($items as $item)
+        <a href="{{ $item['url'] }}" rel="noopener">
+            <x-media.image :src="$item['image']?->getUrl()" :alt="$item['image']?->caption" />
+        </a>
+    @endforeach
+</div>
