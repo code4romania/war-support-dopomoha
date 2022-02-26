5d7eb7fafede3ed9f588228d9b9bb18c34e35ef6
Andrei Ioniță <hi@andrei.io>
2022-02-26 20:23:49 +0000

    feat: banner translations

--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -1,7 +1,7 @@
 <aside class="border-b ">
     <div class="container flex flex-wrap text-sm divide-y lg:justify-end lg:divide-y-none">
         <div class="inline-flex items-center justify-between w-full py-5 lg:w-auto">
-            <span>Un proiect în parteneriat cu</span>
+            <span>@lang('banner.partnership')</span>
             <div class="flex items-center gap-2 pl-4 text-center sm:gap-4">
                 <a href="http://www.dsu.mai.gov.ro/" target="_blank" rel="noopener"
                     class="inline-block focus:outline-none focus:ring">
@@ -26,7 +26,7 @@
             </div>
         </div>
         <div class="inline-flex items-center justify-between w-full py-5 lg:pl-4 lg:w-auto">
-            <span>realizat de</span>
+            <span>@lang('banner.madeby')</span>
             <a href="https://code4.ro" target="_blank" rel="noopener"
                 class="inline-block pl-4 focus:outline-none focus:ring">
                 <img src="{{ asset(mix('assets/images/code4romania.svg')) }}"
