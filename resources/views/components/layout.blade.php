f936b56baf11354b7f0ff4b95335c7fd20258513
Andrei Ioniță <hi@andrei.io>
2022-02-26 20:23:58 +0000

    wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,6 +32,18 @@
     </main>
 
     <x-site.footer />
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8N2MJF8JB2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'G-8N2MJF8JB2');
+    </script>
 </body>
 
 </html>
