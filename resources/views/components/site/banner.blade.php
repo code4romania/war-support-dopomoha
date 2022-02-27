e19bec8af93eb4c57bafe5b5b80746c5b5ac22f3
Andrei Ioniță <hi@andrei.io>
2022-02-27 16:47:25 +0000

    Revert "remove logo"
    This reverts commit 4d2e6a68538f9822ce1863279223e2a5e5428f97.

--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="container flex flex-wrap text-sm divide-y lg:justify-end lg:divide-y-none">
         <div class="inline-flex items-center justify-between w-full py-5 lg:w-auto">
             <span>@lang('banner.partnership')</span>
-            <div class="flex items-center gap-2 pl-4 text-center sm:gap-4">
+            <div class="grid items-center grid-cols-2 gap-2 pl-4 text-center sm:gap-4 md:flex">
                 <a href="http://www.dsu.mai.gov.ro/" target="_blank" rel="noopener"
                     class="inline-block focus:outline-none focus:ring">
                     <img src="{{ asset(mix('assets/images/dsu.png')) }}"
@@ -21,6 +21,13 @@
                     class="inline-block focus:outline-none focus:ring">
                     <img src="{{ asset(mix('assets/images/cnrr.png')) }}"
                         class="inline-block object-contain w-auto h-8"
+                        alt="">
+                </a>
+
+                <a href="https://help.unhcr.org/romania/" target="_blank" rel="noopener"
+                    class="inline-block focus:outline-none focus:ring">
+                    <img src="{{ asset(mix('assets/images/unhcr.svg')) }}"
+                        class="inline-block object-contain w-auto h-6 sm:h-8"
                         alt="">
                 </a>
             </div>
