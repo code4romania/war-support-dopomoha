adc0ace27ef40bfcc210484d1e8768e640f70dda
Andrei Ioniță <hi@andrei.io>
2022-09-28 12:19:55 +0100

    replace banner

--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-gray-100">
     <div class="container flex items-center py-3">
-        <x-icon-code4 class="h-6" />
+        <x-icon-commitglobal class="h-6 sm:h-8 shrink-0" />
 
-        <div class="ml-6 text-sm font-medium text-gray-700">
+        <div class="ml-3 text-sm font-medium text-gray-700">
             <span>@lang('banner.code4')</span>
             <a
-                href="https://www.code4.ro/"
+                href="https://www.commitglobal.org"
                 target="_blank"
                 rel="noopener"
                 class="text-blue-600 hover:underline whitespace-nowrap">
